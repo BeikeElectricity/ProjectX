6ac2014103dfd196dd06172c78636ea9cff0e9b3
Alex Evert <alex.evert@student.gu.se>
2015-09-22 13:10:22 +0200

    Add api wrapper interface.

---
+++ App/app/src/main/java/eic/beike/projectx/eicapi/EICCommunicator.java
@@ -0,0 +1,28 @@
+package eic.beike.projectx.eicapi;
+
+/**
+ * Interface for communicating with the Electricity API.
+ *
+ * Created by alex on 9/22/15.
+ */
+public interface EICCommunicator {
+    /**
+     * Get the latest timestamped data for the active bus.
+     *
+     * @return the sensor data wrapped in a BusData java object.
+     */
+    BusData getBusData();
+
+    /**
+     * Determine which bus the user is currently interested in. This is the active bus.
+     */
+    void determineBus();
+
+    /**
+     * Manually pick a bus as active.
+     *
+     * @param vinNumber the Vin number of the bus that the app should use
+     */
+    void chooseBus(String vinNumber);
+
+}
