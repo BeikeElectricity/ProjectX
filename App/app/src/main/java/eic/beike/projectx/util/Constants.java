526545ab52f1767d73cd73c100935cbd1b1b3363
Alex Evert <alex.evert@student.gu.se>
2015-09-23 00:11:10 +0200

    Add constants class to carry important constants.

---
+++ App/app/src/main/java/eic/beike/projectx/util/Constants.java
@@ -0,0 +1,9 @@
+package eic.beike.projectx.util;
+
+/**
+ * Created by alex on 9/22/15.
+ */
+public class Constants {
+    public static final String BASE_URL = "https://ece01.ericsson.net:4443/ecity";
+    public static final String AUTHORIZATION ="";
+}
