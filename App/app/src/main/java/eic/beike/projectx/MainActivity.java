7d76722e8e18613811e4c0ce1d3ea79889ca2b17
Adam Ingmansson <adam.ingmansson@gmail.com>
2015-09-22 17:58:06 +0200

    Added a check if we have a nick name and saving of the android id

--- App/app/src/main/java/eic/beike/projectx/MainActivity.java
+++ App/app/src/main/java/eic/beike/projectx/MainActivity.java
@@ -1,16 +1,42 @@
 package eic.beike.projectx;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+    public static final String SETTINGS_FILE = "settings";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        SharedPreferences settings = getSharedPreferences(SETTINGS_FILE,0);
+
+        // First time running?
+        if (settings.getString("nick","") == "") {
+            setContentView(R.layout.main_init);
+
+            // Used to fetch the phone id
+            final TelephonyManager tm = (TelephonyManager) getBaseContext()
+                    .getSystemService(Context.TELEPHONY_SERVICE);
+
+            final String androidId;
+            androidId = tm.getDeviceId();
+
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putString("id",androidId);
+
+            editor.commit();
+        } else {
+            setContentView(R.layout.main_splash);
+
+        }
+
     }
 
     @Override
