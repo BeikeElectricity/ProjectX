11c6fec48a51bf2440f9f73bdb46e8092cb5f636
Mikael <mkajsjo2@gmail.com>
2015-09-22 18:25:30 +0200

    Created GameActivity
    Created GameActivity with a basic layout and some dummy buttons.

---
+++ App/app/src/main/java/eic/beike/projectx/GameActivity.java
@@ -0,0 +1,16 @@
+package eic.beike.projectx;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Mikael on 2015-09-22.
+ */
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
