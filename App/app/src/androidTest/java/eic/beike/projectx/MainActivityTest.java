6e4f4c83e0b7d1fb96dd12f5ce11561482fb59f6
Adam Ingmansson <adam.ingmansson@gmail.com>
2015-09-24 20:23:35 +0200

    Added activity tests

---
+++ App/app/src/androidTest/java/eic/beike/projectx/MainActivityTest.java
@@ -0,0 +1,69 @@
+package eic.beike.projectx;
+
+import android.annotation.SuppressLint;
+import android.app.Instrumentation;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.test.UiThreadTest;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.jar.Attributes;
+
+/**
+ * Created by adam on 2015-09-24.
+ */
+public class MainActivityTest
+        extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    MainActivity activity;
+    EditText textField;
+    Button button;
+
+    Context context;
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    @SuppressLint("CommitPrefEdits")
+    public void setUp() throws Exception {
+        super.setUp();
+
+        // Clear name from settings
+        context = getInstrumentation().getContext();
+        SharedPreferences settings = context.getSharedPreferences(MainActivity.SETTINGS_FILE, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.clear().commit();
+
+
+
+
+
+    }
+
+    public void testActivityExists() throws Exception {
+        activity = getActivity();
+        assertNotNull(activity);
+    }
+
+    public void testOnCreate() throws Exception {
+
+        // Used to check if an Activity is started, in this case NameSplashActivity.
+        Instrumentation.ActivityMonitor am =
+                getInstrumentation().addMonitor(
+                        NameSplashActivity.class.getName(), null, true
+                );
+        activity = getActivity();
+
+        // Wait for activity
+        NameSplashActivity nsa = (NameSplashActivity) am.waitForActivityWithTimeout(10000);
+        assertNotNull(nsa);
+        // Check that it was started
+        assertEquals(1,am.getHits());
+    }
+}
---
+++ App/app/src/androidTest/java/eic/beike/projectx/NameSplashActivityTest.java
@@ -0,0 +1,91 @@
+package eic.beike.projectx;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by adam on 2015-09-24.
+ */
+public class NameSplashActivityTest extends ActivityInstrumentationTestCase2<NameSplashActivity> {
+
+    NameSplashActivity activity;
+    EditText textField;
+    Button button;
+
+    Context context;
+
+    public NameSplashActivityTest() {
+        super(NameSplashActivity.class);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+
+
+        // Clear name from settings
+        context = getInstrumentation().getContext();
+        SharedPreferences settings = context.getSharedPreferences(MainActivity.SETTINGS_FILE, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.clear().commit();
+
+        activity = getActivity();
+
+        textField = (EditText) activity.findViewById(R.id.nameInputField);
+        button = (Button) activity.findViewById(R.id.nameOkButton);
+
+
+    }
+
+
+    public void testActivityExists() throws Exception {
+        assertNotNull(activity);
+
+        assertNotNull(textField);
+
+        assertNotNull(button);
+    }
+
+    public void testOnCreate() throws Exception {
+
+    }
+
+    public void testOnClick() throws Exception {
+        String expected = "testName";
+
+        // Send string input value
+        getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                textField.requestFocus();
+            }
+        });
+
+        // Fill in the name
+        getInstrumentation().waitForIdleSync();
+        getInstrumentation().sendStringSync(expected);
+        getInstrumentation().waitForIdleSync();
+
+
+        // Click the button (duh)
+        TouchUtils.clickView(this, button);
+
+        String actualText = textField.getText().toString();
+
+        // Check that the field is filled
+        assertEquals(expected, actualText);
+
+        SharedPreferences settings = context.getSharedPreferences(MainActivity.SETTINGS_FILE,0);
+        String name = settings.getString(MainActivity.NAME_FIELD,"");
+
+        // Check that the name was actually set in settings.
+        assertEquals(expected,name);
+    }
+}
