ea67214b5a258de292b944f6136a2b6be955f0af
Alex Evert <alex.evert@student.gu.se>
2015-09-28 00:16:10 +0200

    Change constants to avoid compiler inlining.
    Makes testing easier as we can use reflection to change
    for example the base url.

--- App/app/src/main/java/eic/beike/projectx/util/Constants.java
+++ App/app/src/main/java/eic/beike/projectx/util/Constants.java
@@ -1,9 +1,14 @@
 package eic.beike.projectx.util;
 
 /**
+ * Global constants.
+ *
+ * Constructor call to avoid compile time inlining making
+ * testing easier (reflect to change possible.)
+ *
  * Created by alex on 9/22/15.
  */
 public class Constants {
-    public static final String BASE_URL = "https://ece01.ericsson.net:4443/ecity";
-    public static final String AUTHORIZATION ="";
+    public static final String BASE_URL = new String("https://ece01.ericsson.net:4443/ecity");
+    public static final String AUTHORIZATION =new String("Z3JwMjU6cnZyMkMqT25kNw==");
 }
