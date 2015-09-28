79fd703edf9d469868f0e2e09c140cb7d3efe1f3
Alex Evert <alex.evert@student.gu.se>
2015-09-28 22:13:18 +0200

    Clean up intermidieate data class.

--- App/app/src/main/java/eic/beike/projectx/busdata/ResponseEntry.java
+++ App/app/src/main/java/eic/beike/projectx/busdata/ResponseEntry.java
@@ -1,26 +1,32 @@
 package eic.beike.projectx.busdata;
 
-import com.google.gson.annotations.SerializedName;
-
 /**
  * Small data class used by gson, comparation on timestamp
  * Created by alex on 9/22/15.
  */
 public class ResponseEntry implements Comparable<ResponseEntry>{
-    @SerializedName("resourceSpec")
-    String resource;
+    /**
+     * The name of the resource in the api.
+     */
+    String resourceSpec;
 
-    @SerializedName("timestamp")
-    Integer timestamp;
+    /**
+     * The timestamp of when the event was recorded on the bus.
+     */
+    long timestamp;
 
-    @SerializedName("value")
+    /**
+     * The value of the sensor.
+     */
     String value;
 
-    @SerializedName("gatewayId")
-    String busVin;
+    /**
+     * The vin number of the bus on which the sensor recorded data.
+     */
+    String gatewayId;
 
     @Override
     public int compareTo(ResponseEntry other){
-        return this.timestamp - other.timestamp;
+        return (int) (this.timestamp - other.timestamp);
     }
 }
