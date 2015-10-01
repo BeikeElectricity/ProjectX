a8d8b4be66f26f08a4c1012d404eabaa5cfd1377
Alex Evert <alex.evert@student.gu.se>
2015-10-01 15:08:34 +0200

    Bugfix - Add missing / ...

--- App/app/src/main/java/eic/beike/projectx/util/Constants.java
+++ App/app/src/main/java/eic/beike/projectx/util/Constants.java
@@ -9,7 +9,7 @@ package eic.beike.projectx.util;
  * Created by alex on 9/22/15.
  */
 public class Constants {
-    public static final String SERVER_URL = new String("http://ec2-52-28-202-131.eu-central-1.compute.amazonaws.com");
+    public static final String SERVER_URL = new String("http://ec2-52-28-202-131.eu-central-1.compute.amazonaws.com/");
     public static final String BASE_URL = new String("https://ece01.ericsson.net:4443/ecity");
     public static final String AUTHORIZATION =new String("Z3JwMjU6cnZyMkMqT25kNw==");
 }
