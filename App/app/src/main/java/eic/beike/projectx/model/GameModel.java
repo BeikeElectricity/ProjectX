e2c9f0dac83d6533f1468e979c18c6451ee08c6b
Mikael <mkajsjo2@gmail.com>
2015-10-01 16:59:42 +0200

    Updated GameModel
    Updated GameModel to determine if score should be given. The calculation
    for score is still needed. Tests still required.

--- App/app/src/main/java/eic/beike/projectx/model/GameModel.java
+++ App/app/src/main/java/eic/beike/projectx/model/GameModel.java
@@ -13,7 +13,8 @@ import eic.beike.projectx.busdata.SimpleBusCollector;
  */
 public class GameModel extends Thread {
 
-    public static final long ONE_SECOND_MILLI = 1000;
+    public static final long ONE_SECOND_IN_MILLI = 1000;
+    public static final long USER_EVENT_EXPIRATION_TIME = 10 * ONE_SECOND_IN_MILLI;
 
     private BusCollector busCollector;
     private List<UserEvent> userEvents;
@@ -27,36 +28,71 @@ public class GameModel extends Thread {
         isRunning = true;
     }
 
+    /**
+     * Lets the thread die a natural way.(not killing it in the middle of a loop)
+     */
     public void stopLoop(){
         isRunning = false;
     }
 
+    /**
+     * Called by the buttons from GameActivity
+     * @param e Sent from the GameActivity when a button is clicked, to be processed in the thread.
+     */
     public void onClick(UserEvent e){
         synchronized (userEvents){
             userEvents.add(e);
         }
     }
 
+    /**
+     * The GameModels main loop which takes user events and gives score based upon them.
+     */
     @Override
     public void run() {
         while (isRunning) {
-            UserEvent e = null;
-            synchronized (userEvents) {
-                if (!userEvents.isEmpty()) {
-                    e = userEvents.remove(0);
+            List<UserEvent> events = getUserEvents();
+            for(UserEvent e : events) {
+                if (isToOld(e)) {
+                    removeUserEvent(e);
+                    //TODO: give negative score
+                    System.out.println("Score: -10");
+                } else if (findMatch(e)) {
+                    removeUserEvent(e);
+                    //TODO: give positive score
+                    System.out.println("Score: +10");
                 }
             }
+            sleepThread(ONE_SECOND_IN_MILLI);
+        }
+    }
 
-            if (e != null) {
-                BusData d = busCollector.getBusData(e.timeStamp, e.sensor);
-                System.out.println(d.getSensor().toString());
-            }
+    private List<UserEvent> getUserEvents(){
+        synchronized (userEvents) {
+            return new ArrayList(userEvents);
+        }
+    }
 
-            try {
-                Thread.sleep(ONE_SECOND_MILLI);
-            } catch (InterruptedException ex) {
-                ex.printStackTrace();
-            }
+    private void removeUserEvent(UserEvent e){
+        synchronized (userEvents) {
+            userEvents.remove(e);
+        }
+    }
+
+    private boolean isToOld(UserEvent e){
+        return System.currentTimeMillis() > e.timeStamp + USER_EVENT_EXPIRATION_TIME;
+    }
+
+    private boolean findMatch(UserEvent e){
+        BusData d = busCollector.getBusData(e.timeStamp, e.sensor);
+        return d.getSensor() == e.sensor;
+    }
+
+    private void sleepThread(long time){
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
         }
     }
 }
