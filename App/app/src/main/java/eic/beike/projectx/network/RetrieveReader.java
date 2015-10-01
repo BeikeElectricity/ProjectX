d90c315a70a5aaa3a4696f794e8ce2272178dd58
Alex Evert <alex.evert@student.gu.se>
2015-10-01 15:09:44 +0200

    Behave differently when handling http and https.

--- App/app/src/main/java/eic/beike/projectx/network/RetrieveReader.java
+++ App/app/src/main/java/eic/beike/projectx/network/RetrieveReader.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Used to retrieve a buffered readers from URLs.
@@ -22,22 +23,31 @@ public class RetrieveReader {
      * @param url the formatted rest call
      * @return an input stream with the server response, this needs to be parsed. NULL if something goes wrong.
      */
-    public static  BufferedReader get(String url) {
+    public static BufferedReader get(String url) {
         BufferedReader in = null;
+
         try {
             URL requestURL = new URL(url);
-            HttpsURLConnection con = (HttpsURLConnection) requestURL.openConnection();
-            con.setRequestMethod("GET");
-            con.setRequestProperty("Authorization", "Basic " + Constants.AUTHORIZATION);
-            int responseCode = con.getResponseCode();
 
-            //Check whether the request was successful.
-            if (responseCode != HttpStatus.SC_OK) {
-                Log.w("RetrieveReader", "Error " + responseCode + " for URL " + url);
-                return null;
+            //Handle https and http connections differently.
+            if (url.startsWith("https")) {
+                HttpsURLConnection con = (HttpsURLConnection) requestURL.openConnection();
+                con.setRequestMethod("GET");
+                con.setRequestProperty("Authorization", "Basic " + Constants.AUTHORIZATION);
+                int responseCode = con.getResponseCode();
+
+                //Check whether the request was successful.
+                if (responseCode != HttpStatus.SC_OK) {
+                    Log.w("RetrieveReader", "Error " + responseCode + " for URL " + url);
+                    return null;
+                }
+                in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else {
+                URLConnection yc = requestURL.openConnection();
+                in = new BufferedReader(
+                        new InputStreamReader(
+                                yc.getInputStream()));
             }
-            in = new BufferedReader(new InputStreamReader(
-                    con.getInputStream()));
         } catch (IOException e) {
             Log.w("RetrieveReader", "Error for URL " + url, e);
         }
