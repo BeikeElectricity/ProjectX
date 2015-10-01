a22e8d0292e04b1aa56b658f0ad5e9a8550d1d1c
Alex Evert <alex.evert@student.gu.se>
2015-10-01 14:25:00 +0200

    Bugfixes in php code.
    Now this script works correctly!

--- server/webservice/Register.php
+++ server/webservice/Register.php
@@ -1,18 +1,21 @@
 <?php
-/* require player and nickname as parameters */
-if(isset($_GET['player']) && intval($_GET['player']) &&
-   isset($_GET['nickname']) &&& strval($_GET['nickname']) {
+/* require id and nickname as parameters */
+if(isset($_GET['id']) && strval($_GET['id']) &&
+   isset($_GET['name']) && strval($_GET['name'])) {
+
+	/* set respone header */
+	header('Content-type: application/json');
 
 	/* parse the input */
-	$player_id = intval($_GET['player']);
-	$nickname = strval($_GET['nickname']);
+	$player_id = strval($_GET['id']);
+	$nickname = strval($_GET['name']);
 
 	/* connect to the db */
 	$link = mysql_connect('localhost','beike','beike') or die('Cannot connect to the DB');
 	mysql_select_db('ProjectX',$link) or die('Cannot select the DB');
 
 	/* insert the user */
-	$query = "INSERT Player (playerId, nickname) VALUES ($player_id,$nickname)";
+	$query = "INSERT INTO Player (playerId, nickname) VALUES ('$player_id','$nickname')";
 	$result = mysql_query($query,$link) or die('Errant query:  '.$query);
 
     /* respond with status */
@@ -26,7 +29,7 @@ if(isset($_GET['player']) && intval($_GET['player']) &&
      } else {
      	// insert failed.
      	$response["success"] = 0;
-     	$response["message"] = "Failed to register!"
+     	$response["message"] = "Failed to register!";
 
         // echoing JSON response
         echo json_encode($response);
