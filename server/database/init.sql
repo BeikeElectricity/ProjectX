c7998d912b7c0cc6c1a4c2ec9fde1711e3ef7053
Alex Evert <alex.evert@student.gu.se>
2015-10-02 00:56:56 +0200

    Make init.sql clear database add user and bus.
    Note that this script drops the database ProjectX if it exists.

--- server/database/init.sql
+++ server/database/init.sql
@@ -1,4 +1,8 @@
+CREATE USER 'beike'@'localhost' IDENTIFIED BY beike;
+GRANT ALL ON ProjectX.* TO 'beike'@'localhost';
+DROP DATABASE IF EXISTS ProjectX;
 CREATE DATABASE ProjectX;
 USE ProjectX;
 SOURCE tables.sql;
 SOURCE procedures.sql;
+SOURCE populate.sql;
