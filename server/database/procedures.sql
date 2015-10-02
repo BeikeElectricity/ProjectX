e8b370858a25d36aa8a0936bd6b52b139e32e8bf
Alex Evert <alex.evert@student.gu.se>
2015-10-02 13:34:22 +0200

    Update procedure to use varchar for player id.

--- server/database/procedures.sql
+++ server/database/procedures.sql
@@ -15,16 +15,16 @@ DELIMITER ;
 -- Select top 10 result for player
 
 DELIMITER //
-CREATE PROCEDURE GetPlayerTopTen (IN player INT)
+CREATE PROCEDURE GetPlayerTopTen (IN player VARCHAR(50))
    BEGIN 
       SELECT nickname, score
       FROM
        (SELECT *
         FROM (Score
-             JOIN Player ON Score.player = Player.playerId)
-        WHERE s.player = player
+             JOIN Player ON Score.player = Player.playerId)) AS S
+        WHERE S.player = player
         ORDER BY score DESC
-        LIMIT 10) AS scoresWithNick;
+        LIMIT 10;
    END
 //
 DELIMITER ;
