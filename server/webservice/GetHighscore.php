a6ed7065dec61ad8d42f09fcb77c03f9db7a0ea7
Alex Evert <alex.evert@student.gu.se>
2015-10-02 13:48:11 +0200

    Bugfixes, now the scripts return the relevant rows

--- server/webservice/GetHighscore.php
+++ server/webservice/GetHighscore.php
@@ -1,6 +1,6 @@
 <?php
 /* If a player was give get the id */
-$player_id = isset($_GET['id']) and strval($_GET['id']) ? strval($_GET['id']) : "";
+$player_id = (isset($_GET['id']) and strval($_GET['id'])) ? strval($_GET['id']) : "";
 
 /* connect to the db */
 $link = mysql_connect('localhost','beike','beike') or die('Cannot connect to the DB');
@@ -8,7 +8,7 @@ mysql_select_db('ProjectX',$link) or die('Cannot select the DB');
 
 /* create a query */
 if(isset($_GET['id'])){
-	$query = "CALL GetPlayerTopTen('$player');";
+	$query = "CALL GetPlayerTopTen('$player_id');";
 } else {
 	$query = "CALL GetGlobalTopTen();";
 }
@@ -16,12 +16,10 @@ if(isset($_GET['id'])){
 /* run the query */
 $result = mysql_query($query,$link);
 
-/* create one master array of the records */
+/* Create array of the relevant rows. */
 $items = array();
-if(mysql_num_rows($result)) {
-	while($row = mysql_fetch_assoc($result)) {
-		$items[] = $row;
-	}
+while($row = mysql_fetch_assoc($result)) {
+	$items[] = $row;
 }
 
 /* output in necessary format */
