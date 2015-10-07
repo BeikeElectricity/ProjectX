e4a11d0d0d7192b213b8bfbecc9c5b5bcad813f2
Alex Evert <alex.evert@student.gu.se>
2015-10-08 01:47:43 +0200

    Make colours carry color codes.

--- App/app/src/main/java/eic/beike/projectx/util/Colour.java
+++ App/app/src/main/java/eic/beike/projectx/util/Colour.java
@@ -1,12 +1,29 @@
 package eic.beike.projectx.util;
 
+
+import eic.beike.projectx.R;
+
 /**
  * Created by Simon on 2015-10-06.
  */
 public enum Colour {
-    GREEN,
-    BLUE,
-    RED;
+
+    GREEN(R.color.green),
+    BLUE(R.color.blue),
+    RED(R.color.red);
+
+    /**
+     * Carried color code initiated at first use.
+     */
+    private  int androidColor;
+    Colour(int androidColor){
+        this.androidColor = androidColor;
+    }
+
+    /**
+     * @return the value of the android color.
+     */
+    public int getAndroidColor(){ return  this.androidColor; }
 
 
     public static Colour colour(int val) {
