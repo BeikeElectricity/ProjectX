45fe9d583a9e8c46203ff107eaff63dff53ae2d0
Alex Evert <alex.evert@student.gu.se>
2015-10-08 10:48:47 +0200

    Update handler to allow new operations.

--- App/app/src/main/java/eic/beike/projectx/handlers/GameHandler.java
+++ App/app/src/main/java/eic/beike/projectx/handlers/GameHandler.java
@@ -7,9 +7,11 @@ import android.os.Message;
 import android.util.Log;
 
 import eic.beike.projectx.activities.GameActivity;
+import eic.beike.projectx.util.Constants;
 
 /**
  * @author Adam
+ * @author Alex
  */
 public class GameHandler extends Handler {
 
@@ -25,10 +27,51 @@ public class GameHandler extends Handler {
         Log.d("Score", Thread.currentThread().getName() + ":handleMessage");
 
         Bundle data = msg.getData();
-        int totalScore = data.getInt("score");
-        int latestScore = data.getInt("latest_score");
 
-        game.showScore(latestScore, totalScore);
+        //See what operation to perform.
+        String operation = data.getString("operation");
+        switch (operation) {
+            case Constants.UPDATESCORE:
+                updateScore(data);
+                break;
+            case Constants.SELECTBUTTON:
+                selectButton(data);
+                break;
+            case Constants.DESELECTBUTTON:
+                deselectButton(data);
+                break;
+            case Constants.UPDATEBOARD:
+                updateBoard(data);
+                break;
+            default:
+                if(operation != null) {
+                    Log.d(getClass().getSimpleName(), "Tried to perform unknown operation!");
+                    break;
+                } else {
+                    Log.d(getClass().getSimpleName(), "Malformed operation message!");
+                    break;
+                }
+        }
     }
 
+
+    private void updateScore(Bundle data){
+        //TODO: Add sanity checks!
+        int totalScore = data.getInt("score");
+        int latestScore = data.getInt("latest_score");
+        int bonusScore = data.getInt("bonus_score");
+        game.updateScore(latestScore, bonusScore, totalScore);
+    }
+
+    private void selectButton(Bundle data){
+        //TODO: Implement this stub!
+    }
+
+    private void deselectButton(Bundle data){
+        //TODO: Implement this stub!
+    }
+
+    private void updateBoard(Bundle data){
+        //TODO: Implement this stub!
+    }
 }
