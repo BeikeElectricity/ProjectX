167f5493ddec64e6828e3ed361b4be33792c41cf
Alex Evert <alex.evert@student.gu.se>
2015-10-08 10:15:31 +0200

    Add constants for passing messages to game ui.

--- App/app/src/main/java/eic/beike/projectx/util/Constants.java
+++ App/app/src/main/java/eic/beike/projectx/util/Constants.java
@@ -3,12 +3,24 @@ package eic.beike.projectx.util;
 /**
  * Global constants.
  *
- * Constructor call to avoid compile time inlining making
+ * Constructor call in network constants to avoid compile time inlining making
  * testing easier (reflect to change possible.)
  *
  * Created by alex on 9/22/15.
  */
 public class Constants {
+    /**
+     * Network constants.
+     */
     public static final String BASE_URL = new String("https://ece01.ericsson.net:4443/ecity");
-    public static final String AUTHORIZATION = new String("Z3JwMjU6cnZyMkMqT25kNw==");
+    public static final String AUTHORIZATION = new String(!!!!!!!!!!!);
+
+    /**
+     * Bundle operation types. Used when passing messages in a bundle to an activity.
+     */
+
+    public static final String UPDATESCORE = "UPDATESCORE";
+    public static final String UPDATEBOARD = "UPDATEBOARD";
+    public static final String SELECTBUTTON = "SELECTBUTTON";
+    public static final String DESELECTBUTTON = "DESELECTBUTTON";
 }
